9389f3a136702293d87214b173d9e17ffe46a3f3
Tzivi <gelsteintzivisantiago@gmail.com>
2021-05-07 11:21:04 -0300

    Solved responsive

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const container = document.getElementById('container')
 const _1 = document.getElementById('_1')
 const _2 = document.getElementById('_2')
 const _3 = document.getElementById('_3')
@@ -7,13 +8,22 @@ const generateRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min)
 }
 
+resize()
+
+window.addEventListener('resize', resize)
+
+function resize() {
+  const containerWidth = container.clientWidth
+  container.style.height = `${containerWidth}px`
+}
+
 setInterval(() => {
   draw()
 }, 400)
 
 const draw = () => {
-  _1.style.boxShadow = `0 0 ${generateRandom(20, 40)}px 2px rgb(226, 42, 18)`
-  _2.style.boxShadow = `0 0 ${generateRandom(20, 40)}px 2px rgb(33, 150, 58)`
-  _3.style.boxShadow = `0 0 ${generateRandom(20, 40)}px 2px rgb(241, 39, 241)`
-  _4.style.boxShadow = `0 0 ${generateRandom(20, 40)}px 2px rgb(41, 67, 216)`
+  _1.style.boxShadow = `0 0 ${generateRandom(10, 40)}px 15px rgb(${generateRandom(220, 255)}, 42, 18)`
+  _2.style.boxShadow = `0 0 ${generateRandom(10, 40)}px 15px rgb(${generateRandom(30, 90)}, 150, 58)`
+  _3.style.boxShadow = `0 0 ${generateRandom(10, 40)}px 15px rgb(${generateRandom(230, 255)}, 39, 241)`
+  _4.style.boxShadow = `0 0 ${generateRandom(10, 40)}px 15px rgb(${generateRandom(30, 80)}, 67, 216)`
 }
